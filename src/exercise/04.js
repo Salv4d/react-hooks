cda345fca73a6cbb02768775ab41c36ba8a9c93e
Salvador Oliveira <salvador.oliveira@pipefy.com>
2022-06-13 23:22:42 -0400

    exercise 04 (hooks) - add game history managing state

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -3,40 +3,21 @@
 
 import * as React from 'react'
 
-function Board() {
-  const [squares, setSquares] = React.useState(
-    () => JSON.parse(localStorage.getItem('squares')) ?? Array(9).fill(null),
-  )
-  const winner = calculateWinner(squares)
-  const nextValue = calculateNextValue(squares)
-  const status = calculateStatus(winner, squares, nextValue)
-
+function Board({selectSquare, currentSquares}) {
   React.useEffect(() => {
-    localStorage.setItem('squares', JSON.stringify(squares))
-  }, [squares])
-
-  function selectSquare(square) {
-    if (winner || squares[square]) return
-    const squaresCopy = [...squares]
-    squaresCopy[square] = nextValue
-    setSquares(squaresCopy)
-  }
-
-  function restart() {
-    setSquares(Array(9).fill(null))
-  }
+    localStorage.setItem('squares', JSON.stringify(currentSquares))
+  }, [currentSquares])
 
   function renderSquare(i) {
     return (
       <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
+        {currentSquares[i]}
       </button>
     )
   }
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -52,18 +33,63 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [currentSquares, setCurrentSquares] = React.useState(
+    () => JSON.parse(localStorage.getItem('squares')) ?? Array(9).fill(null),
+  )
+  const [history, setHistory] = React.useState(
+    () => JSON.parse(localStorage.getItem('history')) ?? [Array(9).fill(null)],
+  )
+
+  React.useEffect(() => {
+    localStorage.setItem('history', JSON.stringify(history))
+  }, [history])
+
+  const winner = calculateWinner(currentSquares)
+  const nextValue = calculateNextValue(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  function restart() {
+    setCurrentSquares(Array(9).fill(null))
+    setHistory([Array(9).fill(null)])
+    localStorage.setItem('history', JSON.stringify([Array(9).fill(null)]))
+  }
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) return
+    const squaresCopy = [...currentSquares]
+    squaresCopy[square] = nextValue
+    setCurrentSquares(squaresCopy)
+
+    setHistory([...history, squaresCopy])
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board
+          history={history}
+          currentSquares={currentSquares}
+          selectSquare={selectSquare}
+        />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol className="history">
+          {history.map((squares, i) => (
+            <button
+              key={`move_${i}`}
+              onClick={() => setCurrentSquares(squares)}
+            >{`Move ${i}`}</button>
+          ))}
+        </ol>
       </div>
     </div>
   )
